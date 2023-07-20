4fd16b528d178094ad12b11601d36b702914500b
JesCab29 <jesusacs2906@gmail.com>
2023-07-20 11:22:08 +0200

    cambio de contador a un añadir a favoritos

--- src/components/shops/ShopCart.jsx
+++ src/components/shops/ShopCart.jsx
@@ -1,23 +1,81 @@
 
 // export default ShopCart
+// import React, { useState } from "react";
+
+// const ShopCart = ({ shopItems, addToCart }) => {
+//   // Creamos un estado que almacene el contador de cantidad para cada producto
+//   const [counts, setCounts] = useState({});
+
+//   const increment = (itemId) => {
+//     setCounts((prevCounts) => ({
+//       ...prevCounts,
+//       [itemId]: (prevCounts[itemId] || 0) + 1, // Si el contador no existe, inicializarlo en 0 y luego incrementar en 1
+//     }));
+//   };
+
+//   return (
+//     <>
+//       {shopItems.map((shopItem) => {
+//         const { id, discount, cover, name, price } = shopItem;
+//         const count = counts[id] || 0; // Obtenemos el contador de cantidad específico para este producto
+
+//         return (
+//           <div className="box" key={id}>
+//             <div className="product mtop">
+//               <div className="img">
+//                 <span className="discount">{discount}% Off</span>
+//                 <img src={cover} alt="" />
+//                 <div className="product-like">
+//                   <label>{count}</label> <br />
+//                   <i className="fa-regular fa-heart" onClick={() => increment(id)}></i>
+//                 </div>
+//               </div>
+//               <div className="product-details">
+//                 <h3>{name}</h3>
+//                 <div className="rate">
+//                   <i className="fa fa-star"></i>
+//                   <i className="fa fa-star"></i>
+//                   <i className="fa fa-star"></i>
+//                   <i className="fa fa-star"></i>
+//                   <i className="fa fa-star"></i>
+//                 </div>
+//                 <div className="price">
+//                   <h4>${price}.00 </h4>
+//                   <button onClick={() => addToCart(shopItem)}>
+//                     <i className="fa fa-plus"></i>
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </>
+//   );
+// };
+
+// export default ShopCart;
 import React, { useState } from "react";
 
 const ShopCart = ({ shopItems, addToCart }) => {
-  // Creamos un estado que almacene el contador de cantidad para cada producto
-  const [counts, setCounts] = useState({});
+  const [favorites, setFavorites] = useState([]);
 
-  const increment = (itemId) => {
-    setCounts((prevCounts) => ({
-      ...prevCounts,
-      [itemId]: (prevCounts[itemId] || 0) + 1, // Si el contador no existe, inicializarlo en 0 y luego incrementar en 1
-    }));
+  const addToFavorites = (itemId) => {
+    if (!favorites.includes(itemId)) {
+      setFavorites([...favorites, itemId]);
+    }
+  };
+
+  const removeFromFavorites = (itemId) => {
+    setFavorites(favorites.filter((id) => id !== itemId));
   };
 
   return (
     <>
       {shopItems.map((shopItem) => {
         const { id, discount, cover, name, price } = shopItem;
-        const count = counts[id] || 0; // Obtenemos el contador de cantidad específico para este producto
+        const isFavorite = favorites.includes(id);
+        const count = isFavorite ? 1 : 0;
 
         return (
           <div className="box" key={id}>
@@ -27,7 +85,11 @@ const ShopCart = ({ shopItems, addToCart }) => {
                 <img src={cover} alt="" />
                 <div className="product-like">
                   <label>{count}</label> <br />
-                  <i className="fa-regular fa-heart" onClick={() => increment(id)}></i>
+                  {isFavorite ? (
+                    <i className="fa-solid fa-heart heart-red" onClick={() => removeFromFavorites(id)}></i>
+                  ) : (
+                    <i className="fa-regular fa-heart" onClick={() => addToFavorites(id)}></i>
+                  )}
                 </div>
               </div>
               <div className="product-details">
