c8457767235388b6065803628d142726d0cb5929
Eva Bustamante <evabrodriguez7@gmail.com>
2023-07-20 11:03:36 +0200

    merge to develop

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import logo from "../../components/assets/images/logo.png"
 import "./style.css"
 
 const Footer = () => {
@@ -7,18 +8,8 @@ const Footer = () => {
       <footer>
         <div className='container grid2'>
           <div className='box'>
-            <h1>WORK HOOD</h1>
+          <img id="logoheader" src={logo} alt='' /> 
             <p>Work Hood, seamos el robin hood de nuestra comunidad, ayudemos a los que lo necesitan.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i className='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i className='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div>
           </div>
 
           <div className='box'>
@@ -42,7 +33,7 @@ const Footer = () => {
           <div className='box'>
             <h2>Contáctanos</h2>
             <ul>
-              <li>C/ de los junior fullstack, Barcelona, 012345, España </li>
+              <li>C/ de los Junior Fullstack, Barcelona, 012345, España </li>
               <li>Email: workhood.help@gmail.com</li>
               <li>Teléfono: +34 123 456 789</li>
             </ul>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,21 +1,16 @@
 import React from "react"
 import Home from "../components/MainPage/Home"
-import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
-import NewArrivals from "../components/newarrivals/NewArrivals"
-import Discount from "../components/discount/Discount"
 import Shop from "../components/shops/Shop"
-import Annocument from "../components/annocument/Annocument"
 import Wrapper from "../components/wrapper/Wrapper"
 
-const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
+const Pages = ({ addToCart, CartItem, shopItems }) => {
   return (
     <>
       <Home CartItem={CartItem} />
       
       <TopCate />
       
-     
       <Shop shopItems={shopItems} addToCart={addToCart} />
       
       <Wrapper />
@@ -24,4 +19,3 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
 }
 
 export default Pages
-// 
